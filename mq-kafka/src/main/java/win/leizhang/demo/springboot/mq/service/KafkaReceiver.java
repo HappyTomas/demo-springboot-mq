30d839a61617fd79498f0a611b7e82a9a9110bd6
zhanglei <zhanglei863@crnet.com.hk>
2018-05-11 12:05:58 +0800

    #13.fine receiver

--- mq-kafka/src/main/java/win/leizhang/demo/springboot/mq/service/KafkaReceiver.java
+++ mq-kafka/src/main/java/win/leizhang/demo/springboot/mq/service/KafkaReceiver.java
@@ -1,6 +1,7 @@
 package win.leizhang.demo.springboot.mq.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -16,15 +17,22 @@ public class KafkaReceiver {
 
     @KafkaListener(topics = {"zhisheng"})
     private void listen(ConsumerRecord<?, ?> record) {
-
+        log.info("----------------- record ==> {}", record);
         Optional<?> kafkaMessage = Optional.ofNullable(record.value());
 
         if (kafkaMessage.isPresent()) {
-
+            // 原对象
             Object obj = kafkaMessage.get();
+            // 转字符串
+            String str = String.valueOf(obj);
 
-            log.info("----------------- record ==> {}", record);
-            log.info("----------------- object ==> {}", obj);
+            // 报文为空，返回
+            if (StringUtils.isBlank(str)) {
+                log.info("message is null, just return!");
+                return;
+            }
+
+            log.info("----------------- string ==> {}", str);
         }
 
     }
