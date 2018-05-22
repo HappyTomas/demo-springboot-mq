c3c75c34cb341363092329c100af97ea0a37d0c0
zhanglei <zhanglei863@crnet.com.hk>
2018-05-22 13:12:51 +0800

    #14.add module activemq

---
+++ mq-activemq/src/main/java/win/leizhang/demo/springboot/mq/bootstrap/config/BaseConfig.java
@@ -0,0 +1,32 @@
+package win.leizhang.demo.springboot.mq.bootstrap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
+
+@Configuration
+public class BaseConfig {
+
+    @Bean
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setThreadNamePrefix("threadPool-");
+        // 基本参数
+        threadPoolTaskExecutor.setCorePoolSize(30);
+        threadPoolTaskExecutor.setMaxPoolSize(80);
+        threadPoolTaskExecutor.setQueueCapacity(1000);
+        threadPoolTaskExecutor.setKeepAliveSeconds(300);
+        // 优雅关闭
+        threadPoolTaskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+        threadPoolTaskExecutor.setAwaitTerminationSeconds(60);
+        // 线程池对拒绝任务（无线程可用）的处理策略
+        RejectedExecutionHandler rejectedExecutionHandler = new CallerRunsPolicy();
+        threadPoolTaskExecutor.setRejectedExecutionHandler(rejectedExecutionHandler);
+        return threadPoolTaskExecutor;
+    }
+
+}
