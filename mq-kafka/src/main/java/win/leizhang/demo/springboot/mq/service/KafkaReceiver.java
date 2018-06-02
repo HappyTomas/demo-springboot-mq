07472a26226589d1d68a058df5e90cabaa9580e4
zhanglei <zhanglei863@crnet.com.hk>
2018-06-02 14:39:47 +0800

    #26.手工签收

--- mq-kafka/src/main/java/win/leizhang/demo/springboot/mq/service/KafkaReceiver.java
+++ mq-kafka/src/main/java/win/leizhang/demo/springboot/mq/service/KafkaReceiver.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -16,7 +17,7 @@ import java.util.Optional;
 public class KafkaReceiver {
 
     @KafkaListener(topics = {"zhang3"})
-    private void listen(ConsumerRecord<?, ?> record) {
+    private void receive(ConsumerRecord<?, ?> record, Acknowledgment ack) {
         //log.debug("----------------- record ==> {}", record);
         log.info("----------------- record ==> detail: topic={}, partition={}, offset={}, timestamp={}, value={}", record.topic(), record.partition(), record.offset(), record.timestamp(), record.value());
 
@@ -26,9 +27,7 @@ public class KafkaReceiver {
         } else {
             String str = String.valueOf(kafkaMessage.get());
             if (StringUtils.isBlank(str) || StringUtils.equals(str, "null") || StringUtils.equals(str, "{}")) {
-                // 报文为空，返回
                 log.warn("message is null, just return!");
-                return;
             } else {
                 // 业务处理和调用
                 log.info("execute logic, the str ==> {}", str);
@@ -36,6 +35,9 @@ public class KafkaReceiver {
             }
         }
 
+        // 手工确认，返回
+        ack.acknowledge();
+        return;
     }
 
 }
