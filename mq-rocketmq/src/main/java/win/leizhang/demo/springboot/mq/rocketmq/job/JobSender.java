f41f53a7928c14f90496d7b1d295b1e404f80722
zhanglei <zhanglei863@crnet.com.hk>
2018-08-29 16:56:45 +0800

    #71.defaultSender

--- mq-rocketmq/src/main/java/win/leizhang/demo/springboot/mq/rocketmq/job/JobSender.java
+++ mq-rocketmq/src/main/java/win/leizhang/demo/springboot/mq/rocketmq/job/JobSender.java
@@ -1,7 +1,43 @@
 package win.leizhang.demo.springboot.mq.rocketmq.job;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import win.leizhang.demo.springboot.mq.rocketmq.mq.RocketmqSender;
+import win.leizhang.demo.springboot.mq.rocketmq.service.bo.MessageBO;
+
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * Created by zealous on 2018/8/29.
  */
+@Slf4j
+@EnableScheduling
+@Component
 public class JobSender {
+
+    @Autowired
+    private RocketmqSender rocketmqSender;
+
+    @Scheduled(cron = "1/5 * * * * ?")
+    public void jobSenderMsg() {
+
+        // 消息对象
+        MessageBO bo = new MessageBO();
+        bo.setId(System.currentTimeMillis());
+        bo.setMsg(UUID.randomUUID().toString());
+        bo.setSendTime(new Date());
+
+        log.info("jobSenderMsg ==> {}", JSON.toJSONString(bo));
+        rocketmqSender.defaultMQProducer("TopicTest", "push", bo);
+
+/*
+        for (int i = 0; i < 10000; i++) {
+        }*/
+    }
+
 }
--- mq-rocketmq/src/main/java/win/leizhang/demo/springboot/mq/rocketmq/mq/RocketmqSender.java
+++ mq-rocketmq/src/main/java/win/leizhang/demo/springboot/mq/rocketmq/mq/RocketmqSender.java
@@ -1,7 +1,71 @@
 package win.leizhang.demo.springboot.mq.rocketmq.mq;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by zealous on 2018/8/29.
  */
+@Slf4j
+@Component
 public class RocketmqSender {
+
+    @Value("${apache.rocketmq.namesrvAddr}")
+    private String namesrvAddr;
+    @Value("${apache.rocketmq.producer.producerGroup}")
+    private String producerGroup;
+
+    //@PostConstruct
+    public void defaultMQProducer(String topic, String tag, Object obj) {
+        //生产者的组名
+        DefaultMQProducer producer = new DefaultMQProducer(producerGroup);
+        //指定NameServer地址，多个地址以 ; 隔开
+        producer.setNamesrvAddr(namesrvAddr);
+
+        String str = JSON.toJSONString(obj);
+        // 业务代码
+        log.debug("准备发mq, topic ==> {}, tag== {}, message ==> {}", topic, tag, str);
+
+        // 消息体
+        Message msg = new Message();
+        msg.setTopic(topic);
+        msg.setTags(tag);
+        try {
+            msg.setBody(str.getBytes(RemotingHelper.DEFAULT_CHARSET));
+        } catch (UnsupportedEncodingException uee) {
+            log.info("不支持的编码转换类型，原文是: {}", str);
+            uee.fillInStackTrace();
+        }
+
+        try {
+            /**
+             * Producer对象在使用之前必须要调用start初始化，初始化一次即可
+             * 注意：切记不可以在每次发送消息时，都调用start方法
+             */
+            producer.start();
+            StopWatch stop = new StopWatch();
+            stop.start();
+
+            // 发
+            SendResult result = producer.send(msg);
+            log.info("结果，发送响应==>{}, 发送状态==>{}", result.getMsgId(), result.getSendStatus());
+
+            stop.stop();
+            log.info("发送耗时:{}", stop.getTotalTimeMillis());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.shutdown();
+        }
+    }
+
 }
